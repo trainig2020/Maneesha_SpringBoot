389bd996016ee36279d2bb292560dcb6863a3b73
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-08-17 22:25:44 +0530

    Add files via upload

---
+++ CourseAPI/src/main/java/com/spring/springbootstarter/CourseApiApp.java
@@ -0,0 +1,14 @@
+package com.spring.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiApp.class, args);
+
+	}
+
+}
